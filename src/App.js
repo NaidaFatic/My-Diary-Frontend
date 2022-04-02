eac4cbeece708662281c7c523372ab5729b7685c
Mirza <krupicmirza99@gmail.com>
2022-04-02 18:44:17 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login";
+
 function App() {
   return (
-    <div>Hello World</div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Login.js
@@ -0,0 +1,5 @@
+function Login() {
+  return <div>Hello world</div>;
+}
+
+export default Login;
