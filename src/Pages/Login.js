e522f31f19c9e5ddb26f614cfe38a04f773febbb
Mirza <krupicmirza99@gmail.com>
2022-04-02 20:16:41 +0200

    login form changes

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,22 @@
+import classes from "./Login.module.css";
+
 function Login() {
-  return <div>Hello world</div>;
+  return (
+    <div className={classes.login_container}>
+      <div className={classes.branding_section}>
+        <h2>
+          Make sure you keep your <br /> dearest moments with
+        </h2>
+        <h1>ONLINE DIARY</h1>
+      </div>
+      <div className={classes.login_form_container}>
+        <form>
+          <input></input>
+          <input></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
