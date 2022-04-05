bc6ef5fa3fe2d4bde8e52ef5801f159bbfff5480
NaidaFatic <faticnaida@gmail.com>
2022-04-06 01:39:31 +0200

    login v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Login from "./Pages/Login";
+import Login from "./Pages/login";
 
 function App() {
   return (
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,29 @@
-import classes from "./Login.module.css";
+import "./login.css";
 
 function Login() {
   return (
-    <div className={classes.login_container}>
-      <div className={classes.branding_section}>
-        <h2>
-          Make sure you keep your <br /> dearest moments with
-        </h2>
-        <h1>ONLINE DIARY</h1>
-      </div>
-      <div className={classes.login_form_container}>
-        <form>
-          <input></input>
-          <input></input>
-        </form>
+    <div className="login_container">
+      <div className="branding_section justify-center fw">
+        <div className="headline">
+          <h2>
+            Make sure you keep your<br /> dearest moments with
+          </h2>
+          <h1 className="mt-0 mb-0 pb-0 logo">Online Diary</h1>
+        </div>
+        <div className="login_form_container self-center">
+          <form className="fw w-410 mx-5">
+            <label>
+              <h4>E-mail</h4>
+              <input type="email" name="email" placeholder="example@example.com" />
+            </label>
+            <label>
+              <h4>Password</h4>
+              <input type="password" name="password" placeholder="strongpassword" />
+            </label>
+            <input type="submit" value="Login" className="mt-5 mb-3" />
+            <h5 className="text-center">don’t have an account?<br /> <a href="" className="fw-700">Register</a></h5>
+          </form>
+        </div>
       </div>
     </div>
   );
