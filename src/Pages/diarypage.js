4b42daa69ce5893e8c6aa446b189771662c70f57
Mirza Krupic <krupicmirza99@gmail.com>
2022-06-11 17:01:55 +0200

    adding vent to callendar

--- src/Pages/diarypage.js
+++ src/Pages/diarypage.js
@@ -1,29 +1,58 @@
 import Calendar from 'react-awesome-calendar';
 import "./diarypage.css"
-import React from 'react';
-
-const events = [{
-    id: 1,
-    color: '#FCC8B2',
-    from: '2022-05-02T18:00:00+00:00',
-    to: '2022-05-05T19:00:00+00:00',
-    title: 'This is an event'
-}, {
-    id: 2,
-    color: '#C6D8AF',
-    from: '2022-05-01T13:00:00+00:00',
-    to: '2022-05-05T14:00:00+00:00',
-    title: 'This is another event'
-}, {
-    id: 3,
-    color: '#FCC8B2',
-    from: '2022-05-05T13:00:00+00:00',
-    to: '2022-05-05T20:00:00+00:00',
-    title: 'This is also another event'
-}];
+import React, { useState } from "react";
+import Modal from '../components/Modal';
 
 
 function DiaryPage(props) {
+    var events = [{
+        id: 1,
+        color: '#FCC8B2',
+        from: '2022-05-02T18:00:00+00:00',
+        to: '2022-05-05T19:00:00+00:00',
+        title: 'This is an event'
+    }, {
+        id: 2,
+        color: '#C6D8AF',
+        from: '2022-05-01T13:00:00+00:00',
+        to: '2022-05-05T14:00:00+00:00',
+        title: 'This is another event'
+    }, {
+        id: 3,
+        color: '#FCC8B2',
+        from: '2022-05-05T13:00:00+00:00',
+        to: '2022-05-05T20:00:00+00:00',
+        title: 'This is also another event'
+    }];
+    const [calEvents, setCalEvents] = useState(<Calendar
+        onClickTimeLine={(event) => {
+            console.log(event)
+            /* result
+               42
+            */
+        }}
+        events={events}
+    />);
+    
+    const addEvent = () =>  {
+        events = [...events, {
+            id: 4,
+            color: '#FCC8B2',
+            from: '2022-06-05T13:00:00+00:00',
+            to: '2022-06-05T20:00:00+00:00',
+            title: 'Test title'
+        }];
+        setCalEvents(<Calendar
+            onClickTimeLine={(event) => {
+                console.log(event)
+                /* result
+                   42
+                */
+            }}
+            events={events}
+        />)
+    }
+
     props.setCurrentPage("DIARY");
     function read(e) {
         console.log('nn');
@@ -31,10 +60,8 @@ function DiaryPage(props) {
 
     return (
         <main className='pt:20 px-4 px:md:0'>
-            <Calendar
-                onclick={read()}
-                events={events}
-            />
+            {calEvents}
+            <Modal addEvent={addEvent} />
         </main>
     );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import DateTimePicker from 'react-datetime-picker';
+
+export default function Modal({addEvent}) {
+  const [showModal, setShowModal] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  return (
+    <>
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Add Event
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Modal Title
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="event" className="sr-only">
+                  Event
+                </label>
+                <input
+                  id="event"
+                  name="event"
+                  type="text"
+                  required
+                  className="appearance-none rounded-none relative block
+                  w-full px-3 py-2 border border-gray-300
+                  placeholder-gray-500 text-gray-900 rounded-t-md
+                  focus:outline-none focus:ring-indigo-500
+                  focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Event"
+                />
+              </div>
+              <p style={{fontSize: '1.2rem', textAlign: 'left', marginTop: '2%'}}>
+                  Start date and time
+                </p>
+              <div>
+                
+                <DateTimePicker id="start" onChange={setStartDate} value={startDate} />
+              </div>
+              <p style={{fontSize: '1.2rem', textAlign: 'left', marginTop: '2%'}}>
+                  End date and time
+                </p>
+              <div>
+                
+                <DateTimePicker id="end" onChange={setEndDate} value={endDate} />
+              </div>
+            </div>
+            
+            
+            <div>
+            <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {addEvent(); setShowModal(false)}}
+                    
+                  >
+                    Save Changes
+                  </button>
+
+            </div>
+          </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
