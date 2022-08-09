04bffb4c887d4bad65f5b1ac1764b04687bdc7b9
NaidaFatic <faticnaida@gmail.com>
2022-08-09 23:59:43 +0200

    header profile id

--- src/Pages/header.js
+++ src/Pages/header.js
@@ -3,12 +3,14 @@ import "../index.css";
 import { IconLayoutDashboard, IconFlame, IconUser, IconBookmark } from '@tabler/icons';
 import { NavLink } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import jwt_decode from "jwt-decode";
 
 function Header() {
     const { logout } = useLogout();
     const Logout = () => {
         logout();
     }
+    var decoded = jwt_decode(localStorage.getItem('token'));
     return (
         <header>
             <div className="fw w-1740 flex justify-between z-50 relative">
@@ -19,11 +21,11 @@ function Header() {
                     <NavLink to="/feed"><IconLayoutDashboard /></NavLink>
                     <NavLink to="/notifications"> <IconFlame /></NavLink>
                     <NavLink to="/diary"><IconBookmark /></NavLink>
-                    <NavLink to="/profile"><IconUser /></NavLink>
+                    <NavLink to={"/profile/" + decoded.uid} ><IconUser /></NavLink>
                     <button onClick={Logout}>Log out</button>
                 </div>
             </div>
-        </header>
+        </header >
     );
 }
 
